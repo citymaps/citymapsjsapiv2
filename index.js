907cc9aad53943757eb7dbbc16a81aa4b8b4a6df
Ben Gundersen <ben@citymaps.com>
2017-08-08 12:10:31 -0400

    leaflet

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * Citymaps JS API v2 library
+ * Based on Leaflet 1.2
+ *
+ * @author bgundersen
+ * @since 6/2017
+ *
+ */
+
+
+'use strict';
+
+var leaflet = require('leaflet');
+
+module.exports = leaflet;
